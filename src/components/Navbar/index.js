a963071fe013c774433612fd77c533ffb444aad8
KTarun003 <tarunreddykreddy@gmail.com>
2020-11-09 23:18:38 +0530

    Another Bug Fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
 
 	componentDidMount() {
     if (this.state.logged_in) {
-      fetch('http://localhost:8000/core/current_user/', {
+      fetch('/core/current_user/', {
         headers: {
           Authorization: `JWT ${localStorage.getItem('token')}`
         }
@@ -30,7 +30,7 @@ class Navbar extends Component {
 
   handle_login = (e, data) => {
     e.preventDefault();
-    fetch('http://localhost:8000/token-auth/', {
+    fetch('/token-auth/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -50,7 +50,7 @@ class Navbar extends Component {
 
   handle_signup = (e, data) => {
     e.preventDefault();
-    fetch('http://localhost:8000/core/users/', {
+    fetch('/core/users/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
