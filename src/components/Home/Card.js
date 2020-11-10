c43272a6f3edd0b3a64d40b5ab7999f3c7578257
KTarun003 <tarunreddykreddy@gmail.com>
2020-11-10 21:01:03 +0530

    Changed Background Colour of Cards

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 
 class Card extends Component {
+	handleClink = () =>{
+		alert("You have clicked on " + this.props.title + " Genre.")
+	}
 	render() {
 		return (
 			<div className="col-md-3">
 				<div className={this.props.colour}>
 					<div className="card-body">
-						<h5>{this.props.title}</h5>
+						<h5><button className="bg-transparent border-0 text-white" onClick={this.handleClink}>{this.props.title}</button> </h5>
 					</div>
 				</div>
 			</div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,22 +11,22 @@ class Index extends Component {
 					<h3 className="text-white">Browse Genres</h3>
 					<hr/>
 					<div className="row" style={{padding:"10px"}}>
-						<Card title={"Romance"} colour={"card text-white bg-night-sky"} />
+						<Card title={"Romance"} colour={"card text-white bg-love-kiss"} />
 						<Card title={"Action & Adventure"} colour={"card text-dark bg-sunny-morning"}/>
-						<Card title={"Mystery & Thriller"} colour={"card text-white bg-grow-early"}/>
-						<Card title={"Biographies & History"} colour={"card text-white bg-malibu-beach"}/>
+						<Card title={"Mystery & Thriller"} colour={"card text-white bg-strong-bliss"}/>
+						<Card title={"Biographies & History"} colour={"card text-white bg-focus"}/>
 					</div>
 					<div className="row" style={{padding:"10px"}}>
 						<Card title={"Children's"} colour={"card text-white bg-night-sky"} />
-						<Card title={"Young Adult"} colour={"card text-dark bg-sunny-morning"}/>
-						<Card title={"Fantasy"} colour={"card text-white bg-grow-early"}/>
-						<Card title={"Historical Fiction"} colour={"card text-white bg-malibu-beach"}/>
+						<Card title={"Young Adult"} colour={"card text-dark bg-mean-fruit"}/>
+						<Card title={"Fantasy"} colour={"card text-white bg-midnight-bloom"}/>
+						<Card title={"Historical Fiction"} colour={"card text-white bg-mixed-hopes"}/>
 					</div>
 					<div className="row" style={{padding:"10px"}}>
-						<Card title={"Horror"} colour={"card text-white bg-night-sky"} />
-						<Card title={"Literary Fiction"} colour={"card text-dark bg-sunny-morning"}/>
-						<Card title={"Non-Fiction"} colour={"card text-white bg-grow-early"}/>
-						<Card title={"Science Fiction"} colour={"card text-white bg-malibu-beach"}/>
+						<Card title={"Horror"} colour={"card text-white bg-plum-plate"} />
+						<Card title={"Literary Fiction"} colour={"card text-dark bg-ripe-malin"}/>
+						<Card title={"Non-Fiction"} colour={"card text-white bg-royal"}/> {/**/}
+						<Card title={"Science Fiction"} colour={"card text-white bg-warm-flame"}/>
 				</div>
 				</div>
 			</div>
