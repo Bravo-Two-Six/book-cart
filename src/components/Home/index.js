f752e943b83bdb2194c47c59af2be726c7f616de
KTarun003 <tarunreddykreddy@gmail.com>
2020-11-11 16:38:25 +0530

    Added Genre Component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import Carousel from "./Carousel";
-import Card from "./Card";
+import Genre from './Genre';
+import Carousel from './Carousel';
+
 class Index extends Component {
 	render() {
 		return (
@@ -8,26 +9,7 @@ class Index extends Component {
 				<Carousel />
 				<br/>
 				<div className="container" >
-					<h3 className="text-white">Browse Genres</h3>
-					<hr/>
-					<div className="row" style={{padding:"10px"}}>
-						<Card title={"Romance"} colour={"card text-white bg-love-kiss"} />
-						<Card title={"Action & Adventure"} colour={"card text-dark bg-sunny-morning"}/>
-						<Card title={"Mystery & Thriller"} colour={"card text-white bg-strong-bliss"}/>
-						<Card title={"Biographies & History"} colour={"card text-white bg-focus"}/>
-					</div>
-					<div className="row" style={{padding:"10px"}}>
-						<Card title={"Children's"} colour={"card text-white bg-night-sky"} />
-						<Card title={"Young Adult"} colour={"card text-dark bg-mean-fruit"}/>
-						<Card title={"Fantasy"} colour={"card text-white bg-midnight-bloom"}/>
-						<Card title={"Historical Fiction"} colour={"card text-white bg-mixed-hopes"}/>
-					</div>
-					<div className="row" style={{padding:"10px"}}>
-						<Card title={"Horror"} colour={"card text-white bg-plum-plate"} />
-						<Card title={"Literary Fiction"} colour={"card text-dark bg-ripe-malin"}/>
-						<Card title={"Non-Fiction"} colour={"card text-white bg-royal"}/> {/**/}
-						<Card title={"Science Fiction"} colour={"card text-white bg-warm-flame"}/>
-				</div>
+					<Genre />
 				</div>
 			</div>
 		);
